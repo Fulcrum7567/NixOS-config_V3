01e8b6d39ec56e64cf1ee5477066a885c27b9eb3
Fulcrum7567 <dragon.fighter@outlook.de>
2025-05-22 22:50:58 +0200

    added conditional flake inputs

--- user/packages/bin/package1/options.nix
+++ user/packages/bin/package1/options.nix
@@ -1,4 +1,8 @@
-{ ... }:
+{ config, lib, ... }:
 {
-	
+	options.custom.hyprland.enable = lib.mkOption {
+		type = lib.types.bool;
+		default = false;
+		description = "Whether to enable hyprland."
+	};
 } 
