58df7b81397c316fa4aee07c35bdef81f3f6fb07
Fulcrum7567 <dragon.fighter@outlook.de>
2025-06-01 21:07:39 +0200

    Changed available themes

--- system/options/themeOptions.nix
+++ system/options/themeOptions.nix
@@ -2,24 +2,17 @@
 {
 	options.theming = {
 
+		availableThemes = lib.mkOption {
+			type = lib.types.listOf lib.types.str;
+			default = [];
+			apply = x: lib.unique x;
+			description = "List of all available themes. Every theme adds itself to this list.";
+		};
+
 		activeTheme = lib.mkOption {
-			type = lib.types.str;
+			type = lib.types.enum (config.theming.availableThemes or []);
 			description = "Set the theme to apply. Must exist in user/themes/profiles/.";
 		};
-
-		isThemeActive = lib.mkOption {
-			type = lib.types.bool;
-			default = false;
-			description = "Option to check whether a valid theme was selected. Is beeing set by the theme.";
-		};
-	};
-
-
-	# Warning
-	config = lib.mkIf (config.theming.isThemeActive == false) {
-		warnings = [
-			"No valid theme is set! Select one from user/themes/profiles and activate it with config.theming.activeTheme."
-		];
 	};
 
 } 
--- user/themes/profiles/demoTheme/config.nix
+++ user/themes/profiles/demoTheme/config.nix
@@ -1,13 +1,11 @@
 { config, lib, ... }:
 {
+
+
 	config = lib.mkIf (config.theming.activeTheme == "demoTheme") {
 		theming = {
-			isThemeActive = true;
 			plymouth.mikuboot.enable = true;
 
 		};
-
-
-
 	};
 }
