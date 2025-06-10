07c844ae34cf5479f60791cdb34755643653aeba
Fulcrum7567 <dragon.fighter@outlook.de>
2025-06-10 15:47:53 +0200

    added S23 to valent

--- user/packages/bin/kdeConnect/configs/valent/config.nix
+++ user/packages/bin/kdeConnect/configs/valent/config.nix
@@ -5,5 +5,25 @@ in
 {
 	config = lib.mkIf (option.enable && (option.activeConfig == "valent")) {
 		programs.kdeconnect.package = pkgs-default.valent;
+
+
+		# Define connected devices
+		home-manager.users.${config.user.settings.username} = {
+			dconf.settings = {
+
+				# S23
+				"ca/andyholmes/valent/device/5ce48006642c4106b12ce8b4ee2b1c94" = {
+					paired = true;
+				};
+
+				/*
+				"ca/andyholmes/valent/device/5ce48006642c4106b12ce8b4ee2b1c94/plugin/clipboard" = {
+					auto-pull = true;
+					auto-push = true;
+				};
+				*/
+
+			};
+		};
 	};
 } 
