9cc61e7e150f4ab2135834c9eff74d95c6a45525
Fulcrum <dragon.fighter@outlook.de>
2025-06-28 12:37:17 +0200

    added dconf

--- user/desktops/environments/gnome/extensions/brightnessControl/configs/default/config.nix
+++ user/desktops/environments/gnome/extensions/brightnessControl/configs/default/config.nix
@@ -7,7 +7,21 @@ in
 		home-manager.users.${config.user.settings.username} = {
 			
 			dconf.settings = {
-
+				"org/gnome/shell/extensions/display-brightness-ddcutil" = {
+					allow-zero-brightness = true;
+					button-location = 1;
+					ddcutil-binary-path = "/run/current-system/sw/bin/ddcutil";
+					ddcutil-queue-ms = 130.0;
+					ddcutil-sleep-multiplier = 40.0;
+					decrease-brightness-shortcut = [ "<Super>Page_Down" ];
+					hide-system-indicator = false;
+					increase-brightness-shortcut = [ "<Super>Page_Up" ];
+					position-system-indicator = 11.0;
+					position-system-menu = 2.0;
+					show-all-slider = false;
+					show-sliders-in-submenu = false;
+					step-change-keyboard = 2.0;
+			    };
 			};
 			
 		};
