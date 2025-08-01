30c9b40f282cfbe41bd7883da458e68a2a38a450
Fulcrum7567 <dragon.fighter@outlook.de>
2025-08-01 16:05:20 +0200

    fixed cube ide

--- user/packages/bin/cubeIDE/package.nix
+++ user/packages/bin/cubeIDE/package.nix
@@ -10,5 +10,25 @@
 				origin = "flathub";
 			}
 		];
+
+		environment.systemPackages = with pkgs-default; [
+			stlink-gui
+			gnumake
+			gnat15
+		];
+
+		services.udev.packages = with pkgs-default; [
+			stlink
+    		openocd
+		];
+
+		users.users.${config.user.settings.username}.extraGroups = [ "plugdev" "dialout" ];
+
+		users.extraGroups.plugdev = {};
+
+		services.udev.extraRules = ''
+		# STM32 ST-Link/V2 (vendor:product 0483:3748)
+		ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE="0666", GROUP="plugdev"
+		'';
 	};
 }
