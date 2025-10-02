b56a11edb30c22350957d2f9bb8e00df9647a780
Fulcrum <dragon.fighter@outlook.de>
2025-10-02 22:21:59 +0200

    Activated num lock on boot

--- user/desktops/displayManagers/gdm/config.nix
+++ user/desktops/displayManagers/gdm/config.nix
@@ -2,5 +2,10 @@
 {
 	config = lib.mkIf (config.displayManagers.activeManager == "gdm") {
 		services.displayManager.gdm.enable = true;
+		programs.dconf.profiles.gdm.databases = [{
+			settings."org/gnome/desktop/peripherals/keyboard" = {
+				numlock-state = true;
+			};
+		}];
 	};
 }
