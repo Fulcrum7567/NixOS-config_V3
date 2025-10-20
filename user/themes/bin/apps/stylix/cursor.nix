0d66064a4fe5654ca730d444b129ca0f46610985
Fulcrum <dragon.fighter@outlook.de>
2025-10-20 14:02:56 +0200

    set theme to null

--- user/themes/bin/apps/stylix/cursor.nix
+++ user/themes/bin/apps/stylix/cursor.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 {
-	config.stylix.cursor = {
+	config.stylix.cursor = lib.mkIf (config.theming.activeTheme != null) {
 		name = config.theming.cursors.name;
 		package = config.theming.cursors.package;
 		size = config.theming.cursors.size;
