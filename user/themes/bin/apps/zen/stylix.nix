1e0e8a2d08b0f7e1305bbb01e82bacf0dd89f59d
Fulcrum <dragon.fighter@outlook.de>
2025-11-20 11:37:20 +0100

    fix

--- user/themes/bin/apps/zen/stylix.nix
+++ user/themes/bin/apps/zen/stylix.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }:
 {
   config = {
-    home-manager.users.${config.user.settings.username}.stylix.targets.zen-browser.profileNames = [ "default" config.user.settings.username ];
+    home-manager.users.${config.user.settings.username}.stylix.targets.zen-browser.profileNames = [ "default" ];
 	};
 }
