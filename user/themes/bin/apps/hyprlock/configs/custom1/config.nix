0aaa923802abe26d637e0e2eb3e99f3449d35fc7
Fulcrum <dragon.fighter@outlook.de>
2026-01-07 16:33:41 +0100

    added fingerprint icon

--- user/themes/bin/apps/hyprlock/configs/custom1/config.nix
+++ user/themes/bin/apps/hyprlock/configs/custom1/config.nix
@@ -1,4 +1,8 @@
 { config, lib, ... }:
+let
+  primaryMonitor = lib.filter (m: m.primary) (lib.attrValues config.hardware.displays);
+  monitor = if primaryMonitor != [] then (lib.head primaryMonitor).name else "";
+in
 {
   config = lib.mkIf (config.theming.apps.hyprlock.enable && (config.theming.apps.hyprlock.activeConfig == "custom1")) {
     home-manager.users.${config.user.settings.username} = {
@@ -9,8 +13,26 @@
         };
       };
 
+      stylix.targets.hyprlock.enable = false;
+
       programs.hyprlock = {
-        settings = lib.mkForce {
+        settings = {
+
+          general = {
+            hide_cursor = true;
+            ignore_empty_password = true;
+          };
+
+          animations = {
+            enabled = true;
+
+            global = {
+              fade = {
+                fadeIn = true;
+                fadeOut = true;
+              };
+            };
+          };
 
           background = [
             {
@@ -30,20 +52,29 @@
             {
               size = "250, 60";
               position = "0, -80";
-              monitor = "";
+              monitor = monitor;
               dots_center = true;
               fade_on_empty = false;
               font_color = "rgb(${config.colorScheme.palette.base05})";
               inner_color = "rgb(${config.colorScheme.palette.base00})";
               outer_color = "rgb(${config.colorScheme.palette.base0D})";
-              outline_thickness = 5;
-              placeholder_text = ''<span foreground="##${config.colorScheme.palette.base05}">Password...</span>'';
+              outline_thickness = 2;
+              placeholder_text = let
+                inherit (config.colorScheme.palette) base05;
+                # Adjust spacing to position icon left and password center
+                pad = "         "; 
+              in 
+                if (config.hosts.components.fingerprint.enable) then
+                  ''<span foreground="##${base05}" font_size="20pt">󰈷</span>${pad}<span foreground="##${base05}" rise='4000'>Password...${pad}</span>''
+                else
+                  ''<span foreground="##${base05}">Password...</span>'';
               shadow_passes = 2;
             }
           ];
 
           label = [
             {
+              monitor = monitor;
               text = "$TIME";
               color = "rgba(${config.colorScheme.palette.base05}ff)";
               font_size = 64;
