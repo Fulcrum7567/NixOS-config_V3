b7ec8072385f2cdf0edb54d964816a64fe87347c
Fulcrum <dragon.fighter@outlook.de>
2026-01-15 17:43:32 +0100

    fixes

--- server/services/reverseProxy/configs/caddy/config.nix
+++ server/services/reverseProxy/configs/caddy/config.nix
@@ -6,11 +6,25 @@
     
     services.caddy = {
       enable = config.server.services.reverseProxy.enable;
+
+      /*
       virtualHosts = lib.mapAttrs' (name: value: lib.nameValuePair value.from {
         extraConfig = ''
           reverse_proxy http://${value.to}
         '';
       }) config.server.services.reverseProxy.activeRedirects;
+      */
+
+      virtualHosts."syncthing.aurek.eu" = {
+        extraConfig = ''
+          # Proxy to the local Syncthing GUI
+          reverse_proxy http://127.0.0.1:8384 {
+              # Syncthing sometimes requires the Host header to match. 
+              # Caddy passes this by default, but we ensure transparency.
+              header_up Host {host}
+          }
+        '';
+      };
     };
   };
 }
