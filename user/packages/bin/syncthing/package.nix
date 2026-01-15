1de9ef97995b2641edc8582416c0719796134c30
Fulcrum <dragon.fighter@outlook.de>
2026-01-15 09:03:52 +0100

    fixes

--- user/packages/bin/syncthing/package.nix
+++ user/packages/bin/syncthing/package.nix
@@ -7,11 +7,19 @@
 			user = "syncthing";
 			group = "syncthing";
 
+			
+
 			settings = {
 				gui = {
 					user = config.user.settings.username;
-        			theme = (if (config.theming.polarity == "light") then "light" else "dark");
+					theme = (if (config.theming.polarity == "light") then "light" else "dark");
 					password = "$2a$10$HWGHFG2AZN3m3bb3OUfyHOoTky57TeC8flop.HfkJqF5UMyD1ha82";
+				};
+
+				devices = {
+					"Server" = {
+						id = "THATUC7-DVGGU6N-3WOFBO5-VKQCPU4-PTRBTZB-4ICONUP-LJXTO36-A43KRAC";
+					};
 				};
 			};
 		};
