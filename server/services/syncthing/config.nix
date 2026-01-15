4a9bb4731476c38fc147ddaf6e661d765c7a34d8
Fulcrum <dragon.fighter@outlook.de>
2026-01-15 08:49:18 +0100

    fixes

--- server/services/syncthing/config.nix
+++ server/services/syncthing/config.nix
@@ -39,7 +39,7 @@
 
         gui = {
           user = config.user.settings.username;
-          theme = if ((config.theming.polarity == "light") then "light" else "dark");
+          theme = (if (config.theming.polarity == "light") then "light" else "dark");
           password = "$2a$10$HWGHFG2AZN3m3bb3OUfyHOoTky57TeC8flop.HfkJqF5UMyD1ha82";
         };
 
