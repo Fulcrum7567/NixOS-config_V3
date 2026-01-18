156816afe00d7e564e532e5b4f06d74667474e4f
Fulcrum <dragon.fighter@outlook.de>
2026-01-18 12:49:38 +0100

    fixes

--- server/services/singleSignOn/configs/kanidm/config.nix
+++ server/services/singleSignOn/configs/kanidm/config.nix
@@ -90,10 +90,12 @@ in
         
         # Kanidm needs a writable HOME for session config
         export HOME=$(mktemp -d)
-
+        
+        echo $ADMIN_PASS_FILE
+        echo $KANIDM
         # Function to check connectivity
         check_status() {
-          tr -d '\n' < cat "$ADMIN_PASS_FILE" | $KANIDM login -H "$KANIDM_URL" --name admin >/dev/null 2>&1
+          tr -d '\n' < "$ADMIN_PASS_FILE" | $KANIDM login -H "$KANIDM_URL" --name admin >/dev/null 2>&1
         }
 
         echo "Waiting for Kanidm to be ready at $KANIDM_URL..."
