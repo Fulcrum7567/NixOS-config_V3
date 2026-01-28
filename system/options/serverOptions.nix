bc1c1b8757ed7a81c94a168b2c27dea08238ced1
Fulcrum <dragon.fighter@outlook.de>
2026-01-28 08:33:12 +0100

    fixes

--- system/options/serverOptions.nix
+++ system/options/serverOptions.nix
@@ -51,7 +51,7 @@
           displayName = config.user.settings.displayName;
           legalName = config.user.settings.displayName;
           mailAddresses = [ "dragon.fighter@outlook.de" ];
-          groups = [ "all_services" "mail-users" ];
+          groups = [ "all_services" ] ++ (if config.server.services.email.enable then [ "mail-users" ] else [ ]);
           present = true;
         };
       };
